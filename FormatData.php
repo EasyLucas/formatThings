a5bb91f2a3e5b557cb9aa16091214373d4de7d6a
EasyLucas <87536653+EasyLucas@users.noreply.github.com>
2022-09-15 15:07:11 -0300

    Create FormatData.php

---
+++ FormatData.php
@@ -0,0 +1,12 @@
+// exemplo de data
+$date = '2022-05-20';
+
+Saída desejada
+Dia/Mês/Ano
+
+echo date('d/m/Y', strtotime($date);
+
+Saída desejada
+Dia-Mês-Ano
+
+echo date('d-m-Y', strtotime($date);
